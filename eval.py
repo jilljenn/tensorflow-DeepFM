fed55768c7c142b6014b5cb2db88fa172425bee2
Jill-Jênn Vie <vie@jill-jenn.net>
2018-05-31 08:31:07 +0900

    Add a column per features

--- eval.py
+++ eval.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import glob
 import json
 import re
@@ -26,12 +27,13 @@ for filename in glob.glob('data/*/*-*-*-*.json'):
         'train_auc': result['metrics'].get('auc_train'),
         'test_auc': result['metrics']['auc_test'],
         'epoch': '{:d}/{:d}'.format(result.get('finished_at_epoch', result['args']['iter']), result['args']['iter']),
-        'dataset': dataset,
+        'dataset': dataset[:dataset.index('_')],
         'data': dataset[-5:],
         'd': 0 if is_lr else result['args']['d'],
         'model': model
     })
-df = pd.DataFrame.from_dict(results).sort_values(by=['data', 'test_auc'], ascending=[True, False]).round(3).fillna('--').reset_index()
+df = pd.DataFrame.from_dict(results).sort_values(by=['data', 'test_auc'], ascending=[True, False]).round(3).reset_index()
+df['first'] = np.where(df['dataset'] == 'first', df['test_auc'], None)
 for data in df['data'].unique():
-    df.query('data == @data')[['dataset', 'model', 'd', 'epoch', 'train_auc', 'test_auc']].to_latex('results-{:s}.tex'.format(data), index=False)
-print(df)
+    df.query('data == @data')[['dataset', 'model', 'd', 'epoch', 'train_auc', 'test_auc', 'first']].to_latex('results-{:s}.tex'.format(data), index=False)
+print(df.fillna('--'))
